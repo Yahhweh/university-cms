024cd68fc1e2f1ad6b8a712a1a1c666e6a6c0a25
Yahhweh <kameniv223@gmail.com>
2026-05-13 23:58:39 +0300

    minor changes

--- src/test/resources/datasets/address_jpa.sql
+++ src/test/resources/datasets/address_jpa.sql
@@ -1,22 +1,19 @@
-INSERT INTO "user" (
-    password,
-    name,
-    sure_name,
-    gender,
-    address_id,
-    date_of_birth,
-    email,
-    role,
-    dtype
-) VALUES (
-             '123',
-             'John',
-             'Doe',
-             '',
-             1,
-             '1995-06-15',
-             'john.doe@student.university.com',
-            'STUDENT',
-          'Student'
-         );
+INSERT INTO "user" (password,
+                    name,
+                    sure_name,
+                    gender,
+                    address_id,
+                    date_of_birth,
+                    email,
+                    role,
+                    dtype)
+VALUES ('123',
+        'John',
+        'Doe',
+        '',
+        1,
+        '1995-06-15',
+        'john.doe@student.university.com',
+        'STUDENT',
+        'Student');
 
